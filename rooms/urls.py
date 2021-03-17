f4c01b82b5b509863e3baca8b7bc38cf5c1c3f92
WitholdP <w.piecewicz@gmail.com>
2021-03-17 13:56:49 +0100

    added form for changing room parameters. Form has same validations as room add

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from rooms.views import AddRoom, Index, RoomDelete, Rooms
+from rooms.views import Index, RoomAdd, RoomDelete, RoomEdit, Rooms
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("rooms/", Rooms.as_view(), name="rooms"),
-    path("add_room/", AddRoom.as_view(), name="add_room"),
+    path("room_add/", RoomAdd.as_view(), name="room_add"),
     path("room_delete/<int:room_id>/", RoomDelete.as_view(), name="room_delete"),
+    path("room_edit/<int:room_id>", RoomEdit.as_view(), name="room_edit"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -28,13 +28,13 @@ class Rooms(View):
         return render(request, "rooms/rooms.html", context)
 
 
-class AddRoom(View):
+class RoomAdd(View):
     """ Displays form for adding a new room to the database. """
 
     def get(self, request):
         message_danger = request.GET.get("message_danger", None)
         return render(
-            request, "rooms/add_room.html", {"message_danger": message_danger}
+            request, "rooms/room_add.html", {"message_danger": message_danger}
         )
 
     def post(self, request):
@@ -95,3 +95,57 @@ class RoomDelete(View):
         message = f"Room {room.room_name} was succesfully deleted"
         room.delete()
         return redirect(reverse("rooms") + f"?message_success={message} ")
+
+
+class RoomEdit(View):
+    """View for editing the specific room. It will aply same form validation as
+    RoomAdd view."""
+
+    def get(self, request, room_id):
+        room = Room.objects.get(pk=room_id)
+        message_success = request.GET.get("message_success", None)
+        message_danger = request.GET.get("message_danger", None)
+        context = {
+            "room": room,
+            "message_success": message_success,
+            "message_danger": message_danger,
+        }
+        return render(request, "rooms/room_edit.html", context)
+
+    def post(self, request, room_id):
+        room_name = request.POST.get("room_name")
+        if not room_name:
+            return redirect(
+                reverse("room_edit", args=[room_id])
+                + "?message_danger=Room name can't be empty"
+            )
+
+        capacity = int(request.POST.get("capacity"))
+        if capacity < 1:
+            return redirect(
+                reverse("room_edit", args=[room_id])
+                + "?message_danger=Capacity has to be more than 0"
+            )
+
+        projector = request.POST.get("projector")
+        if projector:
+            projector = True
+        else:
+            projector = False
+
+        room = Room.objects.get(pk=room_id)
+        room.room_name = room_name
+        room.capacity = capacity
+        room.projector = projector
+        try:
+            room.save()
+        except:
+            return redirect(
+                reverse("room_edit", args=[room_id])
+                + f"?message_danger=Room with name {room_name} already exists"
+            )
+
+        return redirect(
+            reverse("room_edit", args=[room_id])
+            + "?message_success=Room succesfully edited"
+        )
